644ff9d62cb9dc8179e5a6ca35adcc3baa6e2b37
idle sign <idlesign@yandex.ru>
2014-06-03 16:38:31 +0700

    Console app template improved.

--- makeapp/app_templates/console/__module_name__/__init__.py
+++ makeapp/app_templates/console/__module_name__/__init__.py
@@ -9,7 +9,7 @@ def main():
     arg_parser = argparse.ArgumentParser(prog='{{ module_name }}', description='{{ description }}', version='.'.join(map(str, VERSION)))
 
     arg_parser.add_argument('arg1', help='arg1 help')
-    arg_parser.add_argument('--opt', help='optional arg help')
+    arg_parser.add_argument('--opt', help='optional arg help', action='store_true')
 
     parsed_args = arg_parser.parse_args()
     # parsed_args = vars(parsed_args)  # Convert args to dict
