fcb91d78ca34a46f02c3b6a557068ad9dcd291b0
idle sign <idlesign@yandex.ru>
2014-06-03 16:33:54 +0700

    Console app template improved.

--- makeapp/app_templates/console/__module_name__/__init__.py
+++ makeapp/app_templates/console/__module_name__/__init__.py
@@ -6,15 +6,16 @@ VERSION = (0, 1, 0)
 
 def main():
 
-    arg_parser = argparse.ArgumentParser(prog='{{ module_name }}', description='{{ description }}')
+    arg_parser = argparse.ArgumentParser(prog='{{ module_name }}', description='{{ description }}', version='.'.join(map(str, VERSION)))
 
     arg_parser.add_argument('arg1', help='arg1 help')
-    arg_parser.add_argument('--version', action='version', version='%s %s' % ('%(prog)s', '.'.join(map(str, VERSION))))
+    arg_parser.add_argument('--opt', help='optional arg help')
 
     parsed_args = arg_parser.parse_args()
+    # parsed_args = vars(parsed_args)  # Convert args to dict
 
     # Logic goes here.
-    # if parsed_args.version:
+    # if parsed_args['opt']:
 
     sys.exit(1)
 
