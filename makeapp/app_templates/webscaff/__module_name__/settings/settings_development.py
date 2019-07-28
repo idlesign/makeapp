e0e5802916565f09f95134654678c1cbdb80148c
idle sign <idlesign@yandex.ru>
2019-07-28 11:07:25 +0700

    Updated webscaff template.

--- makeapp/app_templates/webscaff/__module_name__/settings/settings_development.py
+++ makeapp/app_templates/webscaff/__module_name__/settings/settings_development.py
@@ -3,6 +3,18 @@ from .settings_base import *
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
-INTERNAL_IPS = ['127.0.0.1']
-ADMINS = (('me', 'me@some.where'),)
+
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    '[::1]',
+]
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+    '[::1]',
+]
+
+ADMINS = (
+    ('me', 'me@some.where'),
+)
--- makeapp/app_templates/webscaff/conf/django.py
+++ makeapp/app_templates/webscaff/conf/django.py
@@ -1,4 +1,12 @@
 # This will be used as Django settings file in production environment.
 from .settings_base import *
 
+
 DEBUG = False
+
+
+ALLOWED_HOSTS = [
+    # Don't forget to put here all IPs and
+    # domain names your is service available at.
+    '{{ webscaff_host }}',
+]
