45d1406a504669162e2c795a0073cf23d647ea19
idle sign <idlesign@yandex.ru>
2019-07-31 18:39:20 +0700

    Template webscaff. uwsgicfg extended.

--- makeapp/app_templates/webscaff/__module_name__/uwsgicfg.py
+++ makeapp/app_templates/webscaff/__module_name__/uwsgicfg.py
@@ -3,15 +3,37 @@ from uwsgiconf.config import configure_uwsgi
 
 def get_configurations():
 
-    from os.path import dirname, abspath, join
+    from pathlib import Path
+    from envbox import get_environment
     from uwsgiconf.presets.nice import PythonSection
 
     section = PythonSection(
-        wsgi_module=join(dirname(abspath(__file__)), 'wsgi.py')
 
-    ).networking.register_socket(
-        PythonSection.networking.sockets.http(':80')
+        wsgi_module='%s' % (Path(__file__).absolute().parent / 'wsgi.py'),
+        log_dedicated=True,
+        ignore_write_errors=True,
+        owner='{{ module_name }}',
+        process_prefix='{{ module_name }}',
+
     )
+    networking = section.networking
+
+    sockets = [
+        networking.sockets.http(':80'),
+    ]
+
+    environ = get_environment()
+    settings = environ.getmany('{{ module_name }}_HTTPS_'.upper())
+
+    https_cert = settings.get('CERT')
+    https_key = settings.get('KEY')
+
+    if https_cert and https_key:
+        sockets.append(
+            networking.sockets.https(':443', cert=https_cert, key=https_key)
+        )
+
+    networking.register_socket(sockets)
 
     return section
 
