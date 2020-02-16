a00d6510ab408ad124d84fee54e3d3993a071662
idle sign <idlesign@yandex.ru>
2020-02-16 19:22:25 +0700

    Fixed tests install as package

--- makeapp/app_templates/__default__/setup.py
+++ makeapp/app_templates/__default__/setup.py
@@ -37,7 +37,7 @@ setup(
     author='{{ author }}',
     author_email='{{ author_email }}',
 
-    packages=find_packages(),
+    packages=find_packages(exclude=['tests']),
     include_package_data=True,
     zip_safe=False,
 
